07ed3a8406c6e5decb525834882d83314553448e
Kage Gamis <kjgamis@gmail.com>
2024-03-25 21:07:52 -0400

    feat: replace logic to Engine functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,81 +1,18 @@
-#include <SFML/Graphics.hpp>
-#include <vector>
-#include <cstdlib> // for rand()
+#include <iostream>
 
-const float xVelocity = 5;
-const float yVelocity = 5;
-const sf::Color gray(128, 128, 128);
-const int windowWidth = 640;
-const int windowLength = 480;
+#include "Engine.h"
+#include "Utils.h"
 
-int main() {
-  sf::RenderWindow window(sf::VideoMode(windowWidth, windowLength), "Dapper Dash");
-  window.setFramerateLimit(24);
+int main(int argc, char** argv) {
 
-  sf::Texture terrier;
-  if (!terrier.loadFromFile("../assets/terrier.png")) {
-    return EXIT_FAILURE;
-  }
-  sf::Texture cookie;
-  if (!cookie.loadFromFile("../assets/cookie.png")) {
-    return EXIT_FAILURE;
-  }
-
-  sf::Sprite dapper;
-  dapper.setTexture(terrier);
-  dapper.setScale(0.25f, 0.25f);
-
-  sf::Vector2 dapperPosition(10.f, 10.f);
-
-  std::vector<sf::Sprite> cookies;
-
-  for (size_t i=0; i<5; i++) {
-    sf::Sprite cookieSprite;
-    cookieSprite.setTexture(cookie);
-    cookieSprite.setScale(0.1f, 0.1f);
-
-    float randX = static_cast<float>(rand() % windowWidth - cookieSprite.getLocalBounds().width * cookieSprite.getScale().x);
-    float randY = static_cast<float>(rand() % windowLength - cookieSprite.getLocalBounds().height * cookieSprite.getScale().y);
-
-    cookieSprite.setPosition(randX, randY);
-    cookies.push_back(cookieSprite);
-  }
-
-  while (window.isOpen()) {
-    sf::Event event{};
-
-    while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
-        window.close();
-      }
+  if (argc > 1) {
+    if (static_cast<std::string>(argv[1])  == "nom") {
+      Engine engine;
+      engine.run();
     }
-
-    dapper.setPosition(dapperPosition);
-
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-      if (dapperPosition.x - xVelocity >= 0) {
-        dapperPosition.x -= xVelocity;
-      }
-    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-      if (dapperPosition.x + xVelocity <= windowWidth - dapper.getLocalBounds().width * dapper.getScale().x) {
-        dapperPosition.x += xVelocity;
-      }
-    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-      if (dapperPosition.y - yVelocity >= 0) {
-        dapperPosition.y -= yVelocity;
-      }
-    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-      if (dapperPosition.y + yVelocity <= windowLength - dapper.getLocalBounds().height * dapper.getScale().y) {
-        dapperPosition.y += yVelocity;
-      }
-    }
-
-    window.clear(gray);
-    for (auto& cookie : cookies) {
-      window.draw(cookie);
-    }
-    window.draw(dapper);
-    window.display();
+  } else {
+    std::cerr << "No arguments provided" << std::endl;
   }
+
   return 0;
 }
